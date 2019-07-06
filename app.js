1a14d8c3203fb8d14253b0aac560cca0eb67b76a
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-07-06 13:18:57 -0700

    merged

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/6.2.4/firebase-app.js"></script>
+
 
 // <!-- TODO: Add SDKs for Firebase products that you want to use
 //      https://firebase.google.com/docs/web/setup#config-web-app -->
@@ -32,9 +31,25 @@ var database = firebase.database();
 // FUNCTIONS + EVENTS
 // --------------------------------------------------------------------------------
 // On Click of Button
+
 $("#add").on("click", function () {
   event.preventDefault();
 
+  // Get the input values
+  var empName = $('#name').val().trim();
+  var empRole = $('#role').val().trim();
+  var empStart = $('#start').val().trim();
+  var empRate = $('#rate').val().trim();
+  var empTotal = empStart * empRate;
+
+
+  database.ref().push({
+    employee: empName,
+    role: empRole,
+    start: empStart,
+    rate: empRate,
+    total: empTotal
+  });
 });
 // MAIN PROCESS + INITIAL CODE
 // --------------------------------------------------------------------------------
@@ -101,67 +116,3 @@ database.ref().once("value", function (snapshot) {
   // In case of error this will print the error
   console.log("The read failed: " + errorObject.code);
 });
-    // VARIABLES
-    // --------------------------------------------------------------------------------
-    // Get a reference to the database service
-    var database = firebase.database();
-    // FUNCTIONS + EVENTS
-    // --------------------------------------------------------------------------------
-    // On Click of Button
-    $("#add").on("click", function() {
-      event.preventDefault();
-
-        // Get the input values
-        var empName = $('#name').val().trim();
-        var empRole = $('#role').val().trim();
-        var empStart = $('#start').val().trim();
-        var empRate = $('#rate').val().trim();
-        var empTotal = empStart * empRate; 
-
-
-      database.ref().push({
-        employee: empName,
-        role: empRole,
-        start: empStart,
-        rate: empRate,
-        total: empTotal
-      });
-    
-
-    
-    });
-    // MAIN PROCESS + INITIAL CODE
-    // --------------------------------------------------------------------------------
-    // Using .on("value", function(snapshot)) syntax will retrieve the data
-    // from the database (both initially and every time something changes)
-    // This will then store the data inside the variable "snapshot". We could rename "snapshot" to anything.
-    var labelText = {
-      "name" : "Employee Name",
-      "role" : "Role",
-      "start" : "Start Date",
-      "worked" : "Months Worked",
-      "rate" : "Monthy Rate ($)",
-      "billed" : "Total Billed ($)"
-    }
-    database.ref().on("value", function(snapshot) {
-      // Then we console.log the value of snapshot
-      console.log(snapshot.val());
-      // Update the clickCounter variable with data from the database.
-      var employees = snapshot.val().employees;
-      // Then we change the html associated with the number.
-      $("#employees").empty();
-      for (var i = 0; i < employees.length; ++i) {
-        var labels = $("<tr>");
-        var data = $("<tr>");
-        for (item in employees[i]) {
-          labels.append("<td>").text(item);
-          data.append("<td>").text(employees[i][item]);
-        }
-        $("#employees").append(labels + data);
-      }
-      // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-      // Again we could have named errorObject anything we wanted.
-    }, function(errorObject) {
-      // In case of error this will print the error
-      console.log("The read failed: " + errorObject.code);
-    });
